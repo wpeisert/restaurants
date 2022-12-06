558522a6d2f7092072daf5715516124c81c7aeba
Wojciech Peisert <wojtekp@sauron.pl>
2022-12-06 21:28:49 +0100

    Category show

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -55,7 +55,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return view('categories.show', compact('category'));
     }
 
     /**
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,51 +1,83 @@
-<x-deals-layout>
+<x-app-layout>
 
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Show Deal (ID: {{ $deal->id }})</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('deals.index') }}"> Deals list</a>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Show category') }}
+        </h2>
+    </x-slot>
+
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+
+                    <section>
+                        <header>
+                            <h2 class="text-lg font-medium text-gray-900">
+                                {{ __('Show category') }}
+                            </h2>
+
+                            <p class="mt-1 text-sm text-gray-600">
+                                {{ __('') }}
+                            </p>
+                        </header>
+
+                        <div class="row">
+                            <x-primary-button><a href="{{ route('categories.index') }}"> &laquo; Categories list
+                                </a></x-primary-button>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Image:</strong>
+                                    {{ $category->image }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Name:</strong>
+                                    {{ $category->name }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Description:</strong>
+                                    {{ $category->description }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Sort by:</strong>
+                                    {{ $category->sortby }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Active:</strong>
+                                    <input type="checkbox" disabled {{ $category->active ? 'checked' : '' }} />
+                                </div>
+                            </div>
+                        </div>
+
+                    </section>
+
+                </div>
             </div>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Description:</strong>
-                {{ $deal->description }}
-            </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <x-deal>
-                    <x-slot name="vulnerable">{{ $deal->vulnerable_human }}</x-slot>
-                    <x-slot name="dealer">{{ $deal->dealer }}</x-slot>
-                    <x-slot name="cards_N">
-                        {!! $deal->getOneLineCards('N') !!}
-                    </x-slot>
-                    <x-slot name="cards_E">
-                        {!! $deal->getOneLineCards('E') !!}
-                    </x-slot>
-                    <x-slot name="cards_S">
-                        {!! $deal->getOneLineCards('S') !!}
-                    </x-slot>
-                    <x-slot name="cards_W">
-                        {!! $deal->getOneLineCards('W') !!}
-                    </x-slot>
-                </x-deal>
-            </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Analysis:</strong><br />
-                {!! nl2br($deal->analysis) !!}
-            </div>
-        </div>
-
-    </div>
-</x-deals-layout>
+</x-app-layout>
